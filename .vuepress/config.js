6258ddd4d9fb02dcc5c730ffc335464ebac8d3b1
jiangshanmeta <540118044@qq.com>
2020-08-04 20:46:55 +0800

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    base:'/notebook/',
+    title:'notebook',
+    dest:'dist',
+    themeConfig:{
+        displayAllHeaders: true ,
+        sidebar:[
+            {
+                title:'高性能网站建设指南',
+                path:'/high-performance-web-site/',
+            },
+            {
+                title:'图解HTTP',
+                path:'/illustration-HTTP/',
+            },
+            {
+                title:'JavaScript函数式编程指南',
+                path:'/functional-programming-in-javascript/',
+            },
+            {
+                title:'JavaScript框架设计',
+                path:'/javascript-framework-design/',
+            },
+            {
+                title:'前端架构设计',
+                path:'/front-end-architecture/'
+            },
+        ]
+    },
+}
